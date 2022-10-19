526d98e337792d00ccf5a5c18e49e63502a1e0cb
Sergey <sergey@MacBook-Pro-Sergey-2.local>
2022-10-19 13:04:09 +0300

    first commit

---
+++ src/main/java/en/serge/springcourse/TestBean.java
@@ -0,0 +1,19 @@
+package en.serge.springcourse;
+
+public class TestBean {
+
+	private String name;
+	
+	public TestBean(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/en/serge/springcourse/TestSpring.java
@@ -0,0 +1,17 @@
+package en.serge.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		TestBean testBean = context.getBean("testBean", TestBean.class);
+		
+		System.out.println(testBean.getName());
+		
+		context.close();
+	}
+
+}
